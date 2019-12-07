0c34797d861949f958f861b438bceb73d6cd0304
Michael Smith <42483787+msmith2777@users.noreply.github.com>
2019-12-06 23:20:04 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MusicPlaylistAnalyzer
+{
+    class Program
+        class ReadFromFile
+    {
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText("SampleMusicPlaylist.txt")
+                var line = reader.ReadLine();
+            var values = line.Split('\t');
+
+            override public string ToString()
+        {
+            return String.Format("Name: {0}, Artist: {1}, Album: {2}, Genre: {3}, Size: {4}, Time: {5}, Year: {6}, Plays: {7}", Name, Artist, Album, Genre, Size, Time, Year, Plays);
+        }
+        Console.WriteLine("Songs that recieve over 200 plays: Name: Losing Touch, Artist: The Killers, Album: Day & Age (Deluxe Version), Genre: Alternative, Size: 9539055, Time: 253, Year: 2008, Plays: 478
+Name: Tranquilize(Feat.Lou Reed), Artist: The Killers, Album: Sawdust, Genre: Alternative, Size: 8405381, Time: 225, Year: 2007, Plays: 433
+Name: “Ruby, Don’t Take Your Love to Town”, Artist: The Killers, Album: Sawdust, Genre: Alternative, Size: 6571031, Time: 185, Year: 2007, Plays: 256")
+            Console.WriteLine("Number of Alternative songs: 39")
+            Console,WriteLine("Number of Hip-Hop/Rap songs: 382")
+            Console,WriteLine("Songs from the album Welcome to the Fishbowl:
+Name: Come Over, Artist: Kenny Chesney, Album: Welcome to the Fishbowl, Genre: Country, Size: 10055955, Time: 249, Year: 2012, Plays: 31
+Name: Feel like a Rock Star (with Tim McGraw), Artist: Kenny Chesney, Album: Welcome to the Fishbowl, Genre: Country, Size: 8419705, Time: 208, Year: 2012, Plays: 23
+Name: Welcome to the Fishbowl, Artist: Kenny Chesney, Album: Welcome to the Fishbowl, Genre: Country, Size: 8487591, Time: 210, Year: 2012, Plays: 10")
+              Console,WriteLine("Songs from before 1970:
+Name: “Hello, I Love You”, Artist: The Doors, Album: The Very Best of The Doors, Genre: Rock, Size: 5438829, Time: 136, Year: 1968, Plays: 5")
+                  Console, WriteLine("Song names longer than 85 characters:
+“Do You Mind (feat.Nicki Minaj, Chris Brown, August Alsina, Jeremih, Future & Rick Ross)”")
+        Console,WriteLine("Longest song: Name: “Devil, Devil (Prelude: Princess of Darkness)”, Artist: Eric Church, Album: The Outsiders, Genre: Country, Size: 19450468, Time: 482, Year: 2014, Plays: 37")
+    }
+    }
+}
